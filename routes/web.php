ce89e8d202a6f57c8630f55aa1d958d2812a24ea
Ib rahim <ibrahimcse.eng@gmail.com>
2021-07-07 22:31:25 +0600

    Blade Templeting And Category crud

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/', function () {
+    return view('website.home');
+})->name('website');
+
+Route::get('/about', function () {
+    return view('website.about');
+});
+
+Route::get('/category', function () {
+    return view('website.category');
+});
+
+Route::get('/contact', function () {
+    return view('website.contact');
+});
+
+Route::get('/post', function () {
+    return view('website.post');
+});
+
+//Admin Panel Routes
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard.index');
+    });
+
+    Route::resource('category', 'CategoryController');
+});
